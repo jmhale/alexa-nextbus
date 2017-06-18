aa77d7d694577e554705ffb0666a0f4473303cc1
James Hale <jmhale08@gmail.com>
2017-06-18 08:28:52 -0400

    Remove extra speechlet builders

--- nextbus_alexa/helpers.py
+++ nextbus_alexa/helpers.py
@@ -31,65 +31,13 @@ def build_event_response(bus_prediction):
                                                bus_prediction['DirectionText'],
                                                arrival_text)
 
-def build_speechlet_response(title, output, reprompt_text, should_end_session):
-    """ Builds a speechlet with a card """
-    return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        },
-        'card': {
-            'type': 'Simple',
-            'title': title,
-            'content': output
-        },
-        'reprompt': {
-            'outputSpeech': {
-                'type': 'PlainText',
-                'text': reprompt_text
-            }
-        },
-        'shouldEndSession': should_end_session
-    }
 
-def build_speechlet_noreprompt(title, output, should_end_session):
+def build_speechlet(output, should_end_session):
     """ Builds a speechlet with a card and without reprompt """
     return {
         'outputSpeech': {
             'type': 'PlainText',
             'text': output
-        },
-        'card': {
-            'type': 'Simple',
-            'title': title,
-            'content': output
-        },
-        'shouldEndSession': should_end_session
-    }
-
-def build_speechlet_noreprompt_nocard(output, should_end_session):
-    """ Builds a speechlet with a card and without reprompt """
-    return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        },
-        'shouldEndSession': should_end_session
-    }
-
-
-def build_speechlet_nocard(output, reprompt_text, should_end_session):
-    """ Builds a speechlet without a card """
-    return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        },
-        'reprompt': {
-            'outputSpeech': {
-                'type': 'PlainText',
-                'text': reprompt_text
-            }
         },
         'shouldEndSession': should_end_session
     }
